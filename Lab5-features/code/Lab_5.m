de188703d93c28c2c4199b8d230d5f7093733db9
Guillaume <g.jeanneret10@uniandes.edu.co>
2017-03-03 18:50:02 -0500

    Lab_5.m file updated

--- Lab5-features/code/Lab_5.m
+++ Lab5-features/code/Lab_5.m
@@ -1,5 +1,5 @@
 %We create the filter bank
-
+tic
 [fb] = fbCreate;
 
 %I load the images
@@ -14,6 +14,8 @@ filterResponses=fbRun(fb,images);
 %%
 %Computer textons from filter
 [map,textons] = computeTextons(filterResponses,k);
+toc
+save 'matrix2.mat'
 
 %%
 
@@ -21,8 +23,6 @@ filterResponses=fbRun(fb,images);
 
 % histos is a variable where each row stores a normalized histogram of the
 % training images.
-addpath('train');
-addpath('test');
 
 histos = [];
 
@@ -46,8 +46,15 @@ end
 
 
 %% Test
-
+tic
 confMat = zeros(25);
+
+class = ones([length(numIm)*25 1]);
+for i=1:25
+
+    class((1+(i-1)*length(numIm)):(i*length(numIm)))=i*ones([length(numIm) 1]);
+  
+end
 
 for i = 1:25
     for j=31:40
@@ -57,32 +64,19 @@ for i = 1:25
         else
             n1 = num2str(i);
         end
-        
         vt1 = assignTextons(fbRun(fb,imread(['test/T' n1 '_' num2str(j) '.jpg'])),textons');
         [~,I] = chiSqDist(histc(vt1(:),1:k)/numel(vt1),histos);
-        if I ~= mod(I,numel(numIm))
-            I = (I-mod(I,numel(numIm)))/numel(numIm);
-        end
-        confMat(I,i)=confMat(I,i)+1;
+        confMat(class(I),i)=confMat(class(I),i)+1;
     end
 end
 confMat = confMat/10;
 
 confianza = sum(diag(confMat))/25;
-
+toc
 %% TreeBagger Test
+tic
 
-%% TreeBagger Test
-
-
-class = ones([length(numIm)*25 1]);
-for i=1:25
-
-    class((1+(i-1)*length(numIm)):(i*length(numIm)))=i*ones([length(numIm) 1]);
-  
-end
-
-nTrees = 50;
+nTrees = 10;
 
 dTree = TreeBagger(nTrees,histos',class','Method', 'classification');
 
@@ -98,9 +92,9 @@ for i = 1:25
         end
         
         vt1 = assignTextons(fbRun(fb,imread(['test/T' n1 '_' num2str(j) '.jpg'])),textons');
-        predict2 = B.predict(histc(vt1(:),1:k)'/numel(vt1));
+        predict2 = dTree.predict(histc(vt1(:),1:k)'/numel(vt1));
 
-        I = str2double(precidt2);
+        I = str2double(predict2);
 
         confMat2(I,i)=confMat2(I,i)+1;
 
@@ -109,6 +103,10 @@ end
 confMat2 = confMat2/10;
 
 confianza2 = sum(diag(confMat2))/25;
+
+toc
+%%
 %%%%%%% Saves the  workspace and the confidence matrix
-save workspace2.mat
+save workspace3.mat
 save confidence.mat confianza
+save confidence2.mat confienza2
