f0aebaf37f45b2be485803262c0a65dd242df962
Cambouu <jm.campo11@uniandes.edu.co>
2017-03-13 16:56:29 -0500

    codigo v4

--- Lab6-Superpixels/segmentByClustering.m
+++ Lab6-Superpixels/segmentByClustering.m
@@ -1,9 +1,18 @@
-function segmentation = segmentByClustering(rgbImage,featureSpace,clusteringMethod,numberOfClusters)
+function segmentation = segmentByClustering(rgbImage,featureSpace,...
+    clusteringMethod,numberOfClusters)
+
+% Verificar que la imagen posea 3 canales
+if size(rgbImage,3)==1
+    rgbImage=repmat(rgbImage,1,1,3);
+end
+
 numberOfClusters=max(2,numberOfClusters);
 
 Precision=8; % bit
 rgbImageN=double(rgbImage)./(2^Precision-1);
 switch featureSpace(1:3)
+    case 'rgb'
+        Im=rgbImageN;
     case 'lab'
         Im=rgb2lab(rgbImageN);
         RLab=rgb2lab(cat(3,1,0,0));
@@ -16,47 +25,53 @@ switch featureSpace(1:3)
     case 'hsv'
         Im=rgb2hsv(rgbImageN);
     otherwise
-        Im=rgbImageN;
+        error('Undefined featureSpace');
 end
 if strcmp(featureSpace(end-2:end),'+xy')
-    [X,Y]=meshgrid((1:size(rgbImage,1))./size(rgbImage,1),...
-        (1:size(rgbImage,2))./size(rgbImage,2));
+    [X,Y]=meshgrid((0:size(rgbImage,2)-1)./(size(rgbImage,2)-1),...
+        (0:size(rgbImage,1)-1)./(size(rgbImage,1)-1));
     Im=cat(3,Im,X,Y);
 end
 
 switch clusteringMethod
     case 'k-means'
-%         ImMat=[];
-%         for i=1:size(Im,3)
-%             Chn=Im(:,:,i);
-%             ImMat=cat(2,Chn(:));
-%         end
         ImMat=reshape(Im,[],size(Im,3));
         Idx=kmeans(ImMat,numberOfClusters);
         segmentation=reshape(Idx,size(Im,1),size(Im,2));
     case 'gmm'
-        ImMat=[];
-        for i=1:size(Im,3)
-            Chn=Im(:,:,i);
-            ImMat=cat(2,Chn(:));
-        end
+        ImMat=reshape(Im,[],size(Im,3));
         GMMModel=fitgmdist(ImMat,numberOfClusters);
         Idx=cluster(GMMModel,ImMat);
         segmentation=reshape(Idx,size(Im,1),size(Im,2));
     case 'hierarchical'
-        ImMat=[];
-        for i=1:size(Im,3)
-            Chn=Im(:,:,i);
-            ImMat=cat(2,Chn(:));
-        end
-        Dist=linkage(pdist(ImMat));
+        ImMat=reshape(Im,[],size(Im,3));
+        Dist=linkage(ImMat,'ward','euclidean','savememory','on');
         Idx=cluster(Dist,'maxclust',numberOfClusters);
         segmentation=reshape(Idx,size(Im,1),size(Im,2));
     case 'watershed'
         ImMat=mean(Im,3);
-        for i=0:1e-1:1
-            
+%         hy=fspecial('sobel');
+%         hx=hy';
+%         ImY=imfilter(ImMat,hy,'replicate');
+%         ImX=imfilter(ImMat,hx,'replicate');
+%         Grad=sqrt(ImX.^2+ImY.^2);
+%         Edge=im2bw(Grad,graythresh(Grad));
+        Edge=edge(ImMat,'Canny');
+        Edge=imdilate(Edge,strel('diamond',1));
+        Label=bwlabel(~Edge);
+        MaxLabel=max(Label(:));
+        Hist=zeros(MaxLabel,1);
+        for i=1:MaxLabel
+            Hist(i)=nnz(Label(:)==i);
         end
+        [~,IdxS]=sort(Hist,'descend');
+        Marker=zeros(size(Label));
+        for i=1:numberOfClusters
+            Marker=or(Marker,Label==IdxS(i));
+        end
+        ImMin=imimposemin(ImMat,Marker);
+        segmentation=watershed(ImMin);
     otherwise
+        error('Undefined clusteringMethod');
 end
 end
