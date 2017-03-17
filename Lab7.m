4bb783e50be786fa0b0562fb7b8aac175da7ac0f
Jorge Madrid <ja.madrid2714@uniandes.edu.co>
2017-03-17 12:16:09 -0500

    Lab7

---
+++ Lab7.m
@@ -0,0 +1,23 @@
+directory = './data/images/*.jpg';
+
+
+Files = dir(directory);
+
+k = [5, 10, 20];
+
+segmentations = cell(numel(Files), numel(k), 2);
+
+for i = 1:length(Files)
+    
+    im = imread(Files(k).name);
+    
+    for j = 1:numel(k)
+        segmentatios[i,j,1} = segmentByClustering(im, 'rgb+xy', 'k-means', k(j));
+        segmentatios[i,j,2} = segmentByClustering(im, 'rgb', 'watershed', k(j));
+        
+   
+    end
+
+end
+
+save('Segmentation_Cell.mat', segmentations); 
