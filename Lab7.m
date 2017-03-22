2a06d0ffff38bb286c9ef43223ad854791fdc14f
Guillaume <g.jeanneret10@uniandes.edu.co>
2017-03-22 09:06:35 -0500

    lab7.m was updated, now works on the bench

--- Lab7.m
+++ Lab7.m
@@ -1,10 +1,11 @@
-directory = './data/images/*.jpg';
-
-
-Files = dir(directory);
+names = {'train' , 'val' , 'test'};
 
 k = [5, 10, 20];
 
+for k2 = 1:3
+
+directory =[ './BSR/BSDS500/data/images/' names{k2} '/*.jpg'];
+Files = dir(directory);
 segmentations = cell(numel(Files), numel(k), 2);
 
 for i = 1:length(Files)
@@ -14,10 +15,14 @@ for i = 1:length(Files)
     for j = 1:numel(k)
         segmentatios{i,j,1} = segmentByClustering(im, 'rgb+xy', 'k-means', k(j));
         segmentatios{i,j,2} = segmentByClustering(im, 'rgb', 'watershed', k(j));
-        
+
    
     end
 
 end
 
-save('Segmentation_Cell.mat', 'segmentations'); 
+save(['Segmentation_cell_' names{k2} '.mat'], 'segmentations');
+
+
+end
+
